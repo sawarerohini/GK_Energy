bbcc6c022cd43169868fbe6059ca56c542d79acc
sawarerohini <132651092+sawarerohini@users.noreply.github.com>
2024-10-23 16:47:37 +0530

    Update test_inventory_request.py

--- testcasses/test_inventory_request.py
+++ testcasses/test_inventory_request.py
@@ -9,7 +9,7 @@ from page_objects.inventory_request import Inventory_Request
 @pytest.fixture(scope="module")
 def setup():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.get('http://erp-gk.gkenergy.in/dashboard/state-management')
+    driver.get('')
     driver.maximize_window()
     login_obj = Login(driver)
     inventory_obj = Inventory_Request(driver)
